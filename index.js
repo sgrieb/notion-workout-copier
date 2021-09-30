54684b48693e6da7edda9710e9ab22a660c061dc
Steve Grieb <sgrieb64@gmail.com>
2021-09-30 14:39:09 -0400

    env and schema copy working

--- index.js
+++ index.js
@@ -9,10 +9,46 @@ const notion = new Client({
 })
 
 async function main() {
-    console.log(process.env.NOTION_TOKEN)
 
-    const listUsersResponse = await notion.users.list()
-    console.log(listUsersResponse)
+    // get base schema
+    const dbSchema = await notion.databases.retrieve({
+      database_id: process.env.BASE_DB_ID,
+    })
+
+    // get contents
+    // const pages = []
+    // let cursor = undefined
+    // while (true) {
+    //   const { results, next_cursor } = await notion.databases.query({
+    //     database_id: BASE_DB_ID,
+    //     start_cursor: cursor,
+    //   })
+
+    //   pages.push(...results)
+    //   if (!next_cursor) {
+    //     break
+    //   }
+    //   cursor = next_cursor
+    // }
+
+    const result = await notion.databases.create({
+      "parent": {
+          "type": "page_id",
+          "page_id": process.env.PARENT_PAGE_ID
+      },
+      "title": [
+        {
+            "type": "text",
+            "text": {
+                "content": "Workout",
+                "link": null
+            }
+        }
+      ],
+      "properties": dbSchema.properties
+    })
+
+    console.log(`result is: ${JSON.stringify(result)}`)
 }
 
 main();
