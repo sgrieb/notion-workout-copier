f80490c133ad04da1e82d61407b8f629be536a99
Steve Grieb <steve.grieb@vts.com>
2022-03-09 20:54:12 -0500

    lint fix

--- src/contents.test.js
+++ src/contents.test.js
@@ -1,27 +1,31 @@
-
-
 const { Client } = require('@notionhq/client');
 
-const { build } = require('./contents')
+const { build } = require('./contents');
 
 describe('build', () => {
-    describe('with valid params', () => {
-        it.only('creates a page', async () => {
-            const config = {
-                uncheckColumns: [
-                    'Difficulty'
-                ],
-                selectColumns: [
-                    'Done'
-                ]
-            }
-            const contentPage = { properties: { 'Done': { select: { options: [] } }, 'Difficulty': { checkbox: {} } } }
-            const dbCreateResult = { ...contentPage }
-            const contentPages = [{ ...contentPage }]
+  describe('with valid params', () => {
+    it('creates a page', async () => {
+      const config = {
+        uncheckColumns: [
+          'Difficulty',
+        ],
+        selectColumns: [
+          'Done',
+        ],
+      };
 
-            const notion = new Client();
+      const contentPage = {
+        properties: {
+          Done: { select: { options: [] } },
+          Difficulty: { checkbox: {} },
+        },
+      };
+      const dbCreateResult = { ...contentPage };
+      const contentPages = [{ ...contentPage }];
 
-            await build(notion, config, dbCreateResult, contentPages)
-        })
-    })
-})
+      const notion = new Client();
+
+      await build(notion, config, dbCreateResult, contentPages);
+    });
+  });
+});
